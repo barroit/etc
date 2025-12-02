dc4555594f1027447cdd8c6f0ff14f0d1311e70b
Jiamu Sun <barroit@linux.com>
2025-12-02 11:06:05 +0900

    scripts: rework mvtree.sh
    Signed-off-by: Jiamu Sun <barroit@linux.com>

--- scripts/mvtree.sh
+++ scripts/mvtree.sh
@@ -11,38 +11,53 @@ else
 	git push
 fi
 
-git add .
-git commit -smTMP
+if [ $(git status --porcelain | wc -l) -eq 0 ]; then
+	exit
+fi
 
 trap 'git reset HEAD^' EXIT
 
+git add .
+git commit -smTMP
+
 if [ $(uname -o) = GNU/Linux ]; then
-	host=dev.macos
+	target=node.raider
 else
-	host=dev.ubuntu
+	target=node.lancer
 fi
 
 upstream=$(git rev-parse --abbrev-ref --symbolic-full-name @{u})
 remote=${upstream%%/*}
 branch=${upstream#*/}
-remote_url=$(git remote get-url $remote)
+repo=$(git remote get-url $remote)
 
-git format-patch HEAD~1 --stdout | ssh $host "
+name=$(basename $PWD)
+dir=$(dirname $PWD)
+
+git format-patch HEAD~1 --stdout | ssh $target "
 set -e
 
-cd \$HOME/${PWD#$HOME/}
-export PATH=\$HOME/.local/bin:\$PATH
+export PATH=\"\$HOME/.local/bin:\$PATH\"
 
-git remote add move-wip $remote_url
+trap 'git remote | grep -xqF mvtree && git remote remove mvtree' EXIT
 
-git fetch move-wip $branch
+cd \$HOME/${dir#$HOME/}
+
+if [ ! -d $name ]; then
+	git clone $repo $name
+fi
+
+cd $name
+
+git remote add mvtree $repo
+git fetch --all
+
 git switch -C $branch
-git reset --hard move-wip/$branch
+git reset --hard mvtree/$branch
 
 git am -
 
 git reset HEAD^
-git remote remove move-wip
 "
 
 trap 'git reset --hard HEAD^' EXIT
