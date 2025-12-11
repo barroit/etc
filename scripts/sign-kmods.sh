11d3ecb3eeb7d3811a835633e4cea35f3a784b4d
Jiamu Sun <barroit@linux.com>
2025-12-12 02:08:08 +0900

    scripts/sign-kmods.sh: sign misc ko as well
    Signed-off-by: Jiamu Sun <barroit@linux.com>

--- scripts/sign-kmods.sh
+++ scripts/sign-kmods.sh
@@ -4,18 +4,25 @@
 set -e
 
 cd /lib/modules/$(uname -r)/updates/dkms
-
-export PATH="/usr/src/linux-headers-$(uname -r)/scripts:$PATH"
+trap 'sudo rm -f *.ko' EXIT
 
 ls | cut -d. -f1-2 | while read name; do
 	sudo zstd -d $name.zst
-
 	sudo /usr/src/linux-headers-$(uname -r)/scripts/sign-file \
 	     sha256 $HOME/.mok/secureboot $HOME/.mok/secureboot.crt $name
-
 	sudo zstd -f $name
-
 done
 
 sudo rm -f *.ko
+trap - EXIT
+
+cd /lib/modules/$(uname -r)/misc
+
+ls | while read name; do
+	sudo /usr/src/linux-headers-$(uname -r)/scripts/sign-file \
+	     sha256 $HOME/.mok/secureboot $HOME/.mok/secureboot.crt $name
+
+	sudo modprobe ${name%.ko}
+done
+
 sudo depmod
